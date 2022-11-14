ca1eb0c79a4174b8ce40b5c2ec4e9fcaa3125bd7
kobe <kobetbuckley@gmail.com>
2022-11-13 16:03:21 -1000

    made the orginal list smaller based on user input for both mood and length

--- randomize_anime.py
+++ randomize_anime.py
@@ -1,54 +1,33 @@
 from random import choice
-
-
-
-# Base Project:
         
-#         1. Put all recommended anime in a list in Python 
-anime = ["naruto", "jujutsu kaisen", "chainsaw man"];
-#         2. Randomly choose one and print
-# print(choice(anime));
-
-
-# base project completed 
-
-
-
-#   Upgraded Project:
-        
-#         1. Make a sublist with anime parameters (nested list) / later replace with variables that are not strings
 animeNested = [
 ["naruto","comedy","long","series"], 
-["jujutsu kaisen","action","short","series"], 
-["chainsaw man","action","short","series"] 
+["jujutsu kaisen","action","short","movie"], 
+["chainsaw man","action","medium","series"] 
 ]
 
-#         2. Use the input function (mood, runtime, series/movie)heavy
 
 mood = input("What mood are u looking for?")
 
-# length = input("What length of anime are u looking for?")
 
-# animeType = input("Are u looking for a series or a movie?")
+for x in animeNested[:]: # removing what does not match from the list 
+    # print(x)
+    if x[1] != mood:
+        animeNested.remove(x)
 
-#         3. Loop to find the mood that matches the input (can be type but maybe a different input )
-
-for x in animeNested:
-    if x[1] == mood: # x is each list, the entire line of code is being recived
-        moodList = x
-        print(moodList)
+print("nested below")
+print(animeNested)   
         
         #! now needing to take that moodList and loop through it to find the length input match.
-        
+length = input("What length are u looking for?")
+
+
+for x in animeNested[:]: # removing what does not match from the list 
+    # print(x)
+    if x[2] != length:
+        animeNested.remove(x)
+
+print("nested below length version")
+print(animeNested)   
         
         #! now needing to take that moodList and loop through it to find the animeType input match.
-        
-        
-        # print(mood + ' anime: ' + x[0]) # print the name of the anime with that mood 
-        
-        
-      
-        
-        
-
-
