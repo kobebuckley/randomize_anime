f4401a5f7e054134feed5853648b16b39f164bae
kobe <kobetbuckley@gmail.com>
2022-11-13 14:37:15 -1000

    added in a mood list that takes the mood selected into a new list of animes that are under that parameter

--- randomize_anime.py
+++ randomize_anime.py
@@ -27,15 +27,23 @@ animeNested = [
 
 mood = input("What mood are u looking for?")
 
-# for x in animeNested:
-#     if x[0][1] == mood:
-#      print(x)
-print(animeNested[0][1])
+# length = input("What length of anime are u looking for?")
+
+# animeType = input("Are u looking for a series or a movie?")
 
 #         3. Loop to find the mood that matches the input (can be type but maybe a different input )
-# for x in animeNested:
-#     if x[0] == mood:
-#         print(mood + ' anime: ' + x[0])
+
+for x in animeNested:
+    if x[1] == mood: # x is each list, the entire line of code is being recived
+        moodList = x
+        print(moodList)
+        
+        
+        # print(mood + ' anime: ' + x[0]) # print the name of the anime with that mood 
+        
+        
+      
+        
+        
 
 
-# print(choice(animeNested[0][0]));
