9f1e1927f08e8101189322382c60366de2d2c91d
kobe <kobetbuckley@gmail.com>
2022-11-13 14:11:34 -1000

    working on the for loop to select the anime with the corresponding mood input

--- randomize_anime.py
+++ randomize_anime.py
@@ -18,15 +18,24 @@ anime = ["naruto", "jujutsu kaisen", "chainsaw man"];
         
 #         1. Make a sublist with anime parameters (nested list) / later replace with variables that are not strings
 animeNested = [
-[["naruto"],["comedy"],["long"],["series"]], 
-[["jujutsu kaisen"],["action"],["short"],["series"]], 
-[["chainsaw man"],["action"],["short"],["series"]] 
+["naruto","comedy","long","series"], 
+["jujutsu kaisen","action","short","series"], 
+["chainsaw man","action","short","series"] 
 ]
 
 #         2. Use the input function (mood, runtime, series/movie)heavy
 
 mood = input("What mood are u looking for?")
-print("Mood selected is", mood)
-#         3. Loop to find the mood that matches the input (can be type but maybe a different input )
 
-print(choice(animeNested[0][0]));
+# for x in animeNested:
+#     if x[0][1] == mood:
+#      print(x)
+print(animeNested[0][1])
+
+#         3. Loop to find the mood that matches the input (can be type but maybe a different input )
+# for x in animeNested:
+#     if x[0] == mood:
+#         print(mood + ' anime: ' + x[0])
+
+
+# print(choice(animeNested[0][0]));
