5287d642df2a24c01cd23aa72d26c7a5b265cff6
kobe <kobetbuckley@gmail.com>
2022-11-13 19:58:39 -1000

    finished finding the most suitable jikan api that is needed to go through the list of animes and select through queries, randomize it through code, etc.,

--- automationAnime.py
+++ automationAnime.py
@@ -1,17 +1,38 @@
 from random import choice
 import requests
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #jikan - anime api
 
-response= requests.get('https://api.jikan.moe/v4/anime/20')
+response= requests.get('https://api.jikan.moe/v4/anime') #! getAnimeSearch on Jikan is the most likely relevant part to continue this project 
 response.status_code
 
 
 
-print(response.status_code)
+# print(response.status_code)
 
 # print(response.json())
 
-malId = response.json()['data']['mal_id']
+# html_text = response,params={'genres'}).text
+# print(html_text)
+
+
+# malId = response.json()['data'][0]['mal_id']
+malId = response.json()['genres']
 print(malId)
+
+
 #pandas - dataframe
